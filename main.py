712b4b1c5f5db4076389f245f1161ebdc42726c1
Krafi <jakub.havlicek5@gmail.com>
2022-02-19 16:22:45 +0100

    Implement image evaluation

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def setup():
     logfile(dir/"kkkm.log")
 
     return sense
-    
+
 
 dt = timedelta(seconds=1)  # Time between measurements
 runtime = timedelta(minutes=178)  # Runtime of the program
@@ -24,7 +24,43 @@ def take_photo():
 
 
 def eval_photo(image):
-    pass   
+    palette = {
+        "ocean": (35, 118, 152),
+        "night": (0, 0, 0),
+        "cloud": (255, 255, 255),
+        "ground": (122, 116, 104),
+    }
+    weights = {
+        "ocean": -0.1,
+        "night": -1.,
+        "cloud": -0.2,
+        "ground": 1.,
+    }
+    counts = {
+        "ocean": 0,
+        "night": 0,
+        "cloud": 0,
+        "ground": 0,
+    }
+
+    # Sampling factor
+    sample = 16
+    for x in range(image.width // sample):
+        for y in range(image.height // sample):
+            pix = image.getpixel((x * sample, y * sample))
+
+            min = 99999
+            color = None
+            for name, col in palette.items():
+                dist = (col[0] - pix[0]) ** 2 + (col[1] -
+                                                 pix[1]) ** 2 + (col[2] - pix[2]) ** 2
+                if dist < min:
+                    min = dist
+                    color = name
+
+            counts[color] += 1
+
+    return round(sum({count * weights[name] for (name, count) in counts.items()})) * sample ** 2
 
 
 def main():
